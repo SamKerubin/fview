8d38417c4b78b04c1e09933209f98ee1aa4a9ddc
SamKerubin <samuelkiller2013@gmail.com>
2025-10-03 23:51:48 -0500

    file listener almost finished

--- listener/file_listener.c
+++ listener/file_listener.c
@@ -17,17 +17,59 @@
 #define OPENED_PATH "/var/log/opfiles"
 #define MODIFIED_PATH "/var/log/modfiles"
 
+#define BUFFER_SIZE 512
+#define FILE_LINE_SIZE 1024
+
 volatile sig_atomic_t running = 1;
 
 struct _file *opened_table;
 struct _file *modified_table;
 
-struct pollfd fds[1];
+struct pollfd fds[2];
 int fan_fd;
 
 /* for SIGTERM */
 void handle_term(const int sig) {
     running = 0;
+}
+
+static char** splitstr(char *str, const char delimiter) {
+    char **result;
+    char *tmp = str;
+    char *last_delim;
+
+    char delim[2];
+    delim[0] = delimiter;
+    delim[1] = '\0';
+
+    size_t count = 0;
+
+    while (*tmp) {
+        if (delimiter == *tmp) {
+            count++;
+            last_delim = tmp;
+        }
+        tmp++;
+    }
+
+    count += last_delim < (str + strlen(str) - 1);
+    count++;
+
+    result = malloc(sizeof(char *) * count);
+    if (result == NULL) {
+        return NULL;
+    }
+
+    size_t ind = 0;
+    char *token = strtok(str, delim);
+    while (token != NULL) {
+        *(result + ind++) = strdup(token);
+        token = strtok(0, delim);
+    }
+
+    *(result + ind) = '\0';
+
+    return result;
 }
 
 static char* getfilepath(const int fd) {
@@ -44,63 +86,99 @@ static char* getfilepath(const int fd) {
 }
 
 static struct _file* loadtable(const char* path) {
-    int fd = open(path, O_RDONLY);
+    struct _file *tmp_table = NULL;
+
+    int fd = open(path, O_RDONLY | O_CREAT, 0644);
     if (fd == -1) {
         syslog(LOG_ERR, "file_listener Error: Couldnt load table from '%s'. -> %s", strerror(errno), path);
         return NULL;
     }
 
-    /* retrieve and parse data allocated in disk */
+    char content[BUFFER_SIZE];
+    char line[FILE_LINE_SIZE];
+    ssize_t bytes_read;
+    int line_pos = 0;
 
-    return NULL;
+    while ((bytes_read = read(fd, content, sizeof(content)) > 0)) {
+        for (ssize_t i = 0; i < bytes_read; i++) {
+            if (content[i] == '\n' || line_pos >= FILE_LINE_SIZE - 1) {
+                line[line_pos] = '\0';
+                char **splitted_line = splitstr(line, ':');
+                if (splitted_line == NULL)
+                    continue;
+
+                char *key = splitted_line[0];
+                char *value_str = splitted_line[1];
+                char *endptr;
+                int value = (int)strtol(value_str, &endptr, 10);
+
+                additem(tmp_table, key, value);
+            }
+        }
+    }
+
+    close(fd);
+    return tmp_table;
 }
 
-static int savetable(const struct _file *table, const char* path) {
+static int savetable(struct _file *table, const char* path) {
     int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd == -1) {
-        syslog(LOG_ERR, "file_listener Error: Coulndt save table into '%s'. -> %s", strerror(errno), path);
+        syslog(LOG_ERR, "file_listener Error: Couldnt save table into '%s'. -> %s", path, strerror(errno));
         perror("open");
         return EXIT_FAILURE;
     }
 
-    /* save table data into disk */
+    struct _file *item, *tmp;
+    HASH_ITER(hh, table, item, tmp) {
+        char *filename = item->key;
+        char count = item->value;
+        char entry[FILE_LINE_SIZE];
 
+        snprintf(entry, sizeof(entry), "%s:%d", filename, count);
+        ssize_t bytes_written = write(fd, entry, strlen(entry));
+        if (bytes_written == -1) {
+            syslog(LOG_ERR, "file_listener Error: Couldnt save table into '%s'. -> %s", path, strerror(errno));
+
+            close(fd);
+            return EXIT_FAILURE;
+        }
+    }
+
+    close(fd);
     return EXIT_SUCCESS;
 }
 
-static void daemonize() {
-    pid_t pid = fork();
+// static void daemonize() {
+//     pid_t pid = fork();
 
-    if (pid < 0) {
-        exit(EXIT_FAILURE);
-    }
+//     if (pid < 0) {
+//         exit(EXIT_FAILURE);
+//     }
 
-    if (pid > 0) {
-        exit(EXIT_SUCCESS);
-    }
+//     if (pid > 0) {
+//         exit(EXIT_SUCCESS);
+//     }
 
-    umask(0);
-    if (chdir("/") < 0) {
-        perror("chdir");
-        exit(EXIT_FAILURE);
-    }
+//     umask(0);
+//     if (chdir("/") < 0) {
+//         perror("chdir");
+//         exit(EXIT_FAILURE);
+//     }
 
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
+//     close(STDIN_FILENO);
+//     close(STDOUT_FILENO);
+//     close(STDERR_FILENO);
 
-    if (setsid() < 0) {
-        exit(EXIT_FAILURE);
-    }
+//     if (setsid() < 0) {
+//         exit(EXIT_FAILURE);
+//     }
 
-    int fd = open("/dev/null", O_RDWR);
-    dup2(fd, STDIN_FILENO);
-    dup2(fd, STDOUT_FILENO);
-    dup2(fd, STDERR_FILENO);
-
-    openlog("file_listener", LOG_PID | LOG_CONS, LOG_DAEMON);
-    syslog(LOG_INFO, "file_listener Daemon started.");
-}
+//     int fd = open("/dev/null", O_RDWR);
+//     dup2(fd, STDIN_FILENO);
+//     dup2(fd, STDOUT_FILENO);
+//     dup2(fd, STDERR_FILENO);
+// }
 
 static void set_timer(void) {
     int tfd = timerfd_create(CLOCK_MONOTONIC, 0);
@@ -120,8 +198,10 @@ static void set_timer(void) {
         exit(EXIT_FAILURE);
     }
 
-    fds[0].fd = tfd;
+    fds[0].fd = fan_fd;
     fds[0].events = POLLIN;
+    fds[1].fd = tfd;
+    fds[1].events = POLLIN;
 }
 
 static void init_fanotify(const char *path) {
@@ -136,7 +216,7 @@ static void init_fanotify(const char *path) {
                         FAN_OPEN | FAN_MODIFY | FAN_EVENT_ON_CHILD,
                         AT_FDCWD,
                         path) == -1) {
-        syslog(LOG_ERR, "file_listener Error: Couldnt mark mount point to fanotify -> '%s'", path);
+        syslog(LOG_ERR, "file_listener Error: Couldnt mark mount point to fanotify in '%s' -> %s", path, strerror(errno));
         exit(EXIT_FAILURE);
     }
 }
@@ -153,63 +233,64 @@ static int path_in_blacklist(const char* path) {
 
 static void loop(void) {
     while(running) {
-        struct fanotify_event_metadata buffer[200];
-        ssize_t len = read(fan_fd, buffer, sizeof(buffer));
-
-        if (len < 0 && errno != EAGAIN) {
-            syslog(LOG_ERR, "file_listener Error: Couldnt read event metadata from file descriptior '%d'. -> %s", fan_fd, strerror(errno));
-            break;
-        }
-
-        if (len <= 0) 
+        int ret = poll(fds, 1, -1);
+        if (ret == -1) {
+            perror("poll");
             continue;
-
-        struct fanotify_event_metadata *meta;
-        struct _file *tmp_table = NULL;
-        char *tmp_path = NULL;
-        for (meta = buffer; FAN_EVENT_OK(meta, len); meta = FAN_EVENT_NEXT(meta, len)) {
-            if (meta->mask & FAN_OPEN || meta->mask & FAN_MODIFY) {
-                char realpath[PATH_LENGTH];
-                const char *filepath = getfilepath(meta->fd);
-                if (filepath == NULL) {
-                    close(meta->fd);
-                    continue;
-                }
-
-                strcpy(realpath, filepath);
-
-                if (path_in_blacklist(realpath))
-                    continue;
-
-                if (meta->mask & FAN_OPEN) {
-                    tmp_table = opened_table;
-                    tmp_path = OPENED_PATH;
-                } else {
-                    tmp_table = modified_table;
-                    tmp_path = MODIFIED_PATH;
-                }
-
-                struct _file *file_affected = getitem(tmp_table, realpath);
-                int count = file_affected == NULL ? 1 : file_affected->value + 1;
-                additem(tmp_table, realpath, count);
-
-            }
-            int ret = poll(fds, 1, -1);
-            if (ret != -1) {
-                perror("poll");
-                continue;
-            }
-
-            if (fds[0].events & POLLIN) {
-                savetable(opened_table, OPENED_PATH);
-                savetable(modified_table, MODIFIED_PATH);
-            }
-
-            tmp_table = NULL;
-            tmp_path = NULL;
-
-            close(meta->fd);
         }
+        
+        if (fds[0].events & POLLIN) {
+            struct fanotify_event_metadata buffer[200];
+            ssize_t len = read(fan_fd, buffer, sizeof(buffer));
+
+            if (len < 0 && errno != EAGAIN) {
+                syslog(LOG_ERR, "file_listener Error: Couldnt read event metadata from file descriptior '%d'. -> %s", fan_fd, strerror(errno));
+                break;
+            }
+
+            if (len <= 0) 
+                continue;
+
+            struct fanotify_event_metadata *meta;
+            struct _file *tmp_table = NULL;
+            char *tmp_path = NULL;
+            for (meta = buffer; FAN_EVENT_OK(meta, len); meta = FAN_EVENT_NEXT(meta, len)) {
+                if (meta->mask & FAN_OPEN || meta->mask & FAN_MODIFY) {
+                    const char *filepath = getfilepath(meta->fd);
+                    if (filepath == NULL) {
+                        close(meta->fd);
+                        continue;
+                    }
+
+                    if (path_in_blacklist(filepath))
+                        continue;
+
+                    if (meta->mask & FAN_OPEN) {
+                        tmp_table = opened_table;
+                        tmp_path = OPENED_PATH;
+                    } else {
+                        tmp_table = modified_table;
+                        tmp_path = MODIFIED_PATH;
+                    }
+
+                    struct _file *file_affected = getitem(tmp_table, filepath);
+                    int count = file_affected == NULL ? 1 : file_affected->value + 1;
+                    additem(tmp_table, filepath, count);
+
+                }
+
+                tmp_table = NULL;
+                tmp_path = NULL;
+
+                close(meta->fd);
+            }
+        }
+
+        if (fds[1].events & POLLIN) {
+            savetable(opened_table, OPENED_PATH);
+            savetable(modified_table, MODIFIED_PATH);
+        }
+
     }
 }
 
@@ -224,9 +305,12 @@ static void clean_loop(void) {
 }
 
 int main(void) {
-    daemonize();
+    // daemonize();
 
     signal(SIGTERM, handle_term);
+
+    openlog("file_listener", LOG_PID | LOG_CONS, LOG_DAEMON);
+    syslog(LOG_INFO, "file_listener Daemon started.");
 
     set_timer();
 
